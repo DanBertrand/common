e6751f83281457b725cf5ed990c82f7c1465e8a8
Dan Bertrand <dan.bertrand@azeti.net>
2022-02-13 12:20:46 +0100

    Modify dist folder

--- dist/bin/merge.sh
+++
@@ -1,8 +0,0 @@
-# Absolute path to this script, e.g. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f "$0")
-echo "Absolute path to this script" $SCRIPTPATH
-# Absolute path this script is in, thus /home/user/bin
-SCRIPTPATH=$(dirname "$SCRIPT")
-echo "Absolute path this script is in" $SCRIPTPATH
-
-node merge.js $SCRIPTPATH
--- dist/index.ts
+++
@@ -1,8 +0,0 @@
-import tsconfig from "./tsconfig.json";
-import eslintConfig from "./.eslintrc.json";
-
-export { tsconfig, eslintConfig };
-
-const hello = (): number => {
-  return 2;
-};
