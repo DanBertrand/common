f619cf882ad021e2f37b61f4241550558b30de03
Dan Bertrand <dan.bertrand@azeti.net>
2022-02-14 15:47:47 +0100

    get path from local package.json

--- merge.js
+++ merge.js
@@ -1,10 +1,26 @@
 const fs = require("fs");
-const localePackagePath = "../../../package.json";
+const path = require("path");
+const absolutePath = path.dirname(__dirname);
 
-if (!fs.existsSync(localePackagePath)) return;
+const getLocalPackageJsonPath = (absolutePath) => {
+  const dirs = absolutePath.split("/");
+  const lastIncludedDir = dirs[dirs.indexOf("node_modules") - 1]; // last included directory; directory BEFORE node_modules
+  let localPackagePath = "";
+  dirs.some((dir) => {
+    localPackagePath += `${dir}`;
+    return dir === lastIncludedDir;
+  });
+
+  return localPackagePath + "/package.json";
+};
+
+const packageJsonPath = getLocalPackageJsonPath(absolutePath);
+console.log("packageJsonPath", packageJsonPath);
+
+if (!fs.existsSync(packageJsonPath)) return;
 
 const sharedPackage = require("./package.json");
-const localPackage = require(localePackagePath);
+const localPackage = require(packageJsonPath);
 
 // Empty or create the peerDependencies
 localPackage.peerDependencies = {};
@@ -20,14 +36,14 @@ if (!localPackage.devDependencies) {
 // Merge devDependencies to the the locale package.json devDependencies when the package is downloaded
 // const pattern = /\b(eslint)\b/g; // <-- If package belong to eslint, we want to import it to the other packages.json
 for (const [key, value] of Object.entries(sharedPackage.devDependencies)) {
-  console.log("Value", value);
   if (key.includes("eslint")) {
     localPackage.devDependencies[key] = value;
   }
 }
 
 // Write the file with the modifications
-fs.writeFileSync(localePackagePath, JSON.stringify(localPackage, null, 2));
+fs.writeFileSync(packageJsonPath, JSON.stringify(localPackage, null, 2));
+
 // prettier condig + extend eslint
 //  name of the plugin
 
