1dd5a7a84559018eff3fa4b390cdc6756dffc0e3
Dan Bertrand <dan.bertrand@azeti.net>
2022-02-15 10:06:55 +0100

    export prettier config

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import tsconfig from "./tsconfig.json";
 import eslintConfig from "./.eslintrc.json";
+import prettierConfig from "./.prettierrc.json";
 
-export { tsconfig, eslintConfig };
+export { tsconfig, eslintConfig, prettierConfig };
 
 const hello = (): number => {
   return 2;
