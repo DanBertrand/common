504bb0690ab04859ff93bac79ed0a3f05bd2f5d9
Dan Bertrand <dan.bertrand@azeti.net>
2022-02-15 10:22:27 +0100

    Correted prettierrc for import

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import tsconfig from "./tsconfig.json";
 import eslintConfig from "./.eslintrc.json";
-import prettierConfig from "./.prettierrc.json";
 
-export { tsconfig, eslintConfig, prettierConfig };
+export { tsconfig, eslintConfig };
 
 const hello = (): number => {
   return 2;
