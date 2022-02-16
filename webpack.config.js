0cb587d7af8af435d5a78e4fc4347a286044cec8
Dan Bertrand <dan.bertrand@azeti.net>
2022-02-16 12:27:13 +0100

    test building

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
-  entry: "./index.ts",
+  entry: ".",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
